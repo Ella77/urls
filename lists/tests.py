490b5727d79143b4b1962fc34e4d44fb5288e86d
Ella77 <ella@mykoon.com>
2018-08-03 17:31:29 +0900

    url mapping
    url mapping

---
+++ lists/tests.py
@@ -0,0 +1,14 @@
+from django.urls import resolve
+from django.test import TestCase
+
+# from . import views
+
+from lists.views import home_page
+
+class HomePageTest(TestCase):
+
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func,home_page)
+# Create your tests here.
